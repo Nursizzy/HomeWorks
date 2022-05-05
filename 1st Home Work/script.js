fc26caf0cee6077d96690c0c606258d9b17a31b5
Nursizzy <101743904+Nursizzy@users.noreply.github.com>
2022-05-05 15:21:59 +0600

    Update script.js

--- 1st Home Work/script.js
+++ 1st Home Work/script.js
@@ -13,7 +13,7 @@ FIRST_TASK_BUTTON.addEventListener('click', function () {
   const numSystemIsCorrect = numSystem >= 2 && numSystem <= 36;
   
   if (isFinite(num) === isFinite(numSystem) && numSystemIsCorrect) 
-    console.log(`Ответ: ${num.toString(numSystem)}`);
+    console.log(`Результат: ${num.toString(numSystem)}`);
   else errorLog();
 });
 
