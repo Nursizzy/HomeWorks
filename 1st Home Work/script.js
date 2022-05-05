435289624f8e7c38d0bccaa99f38ec3cfa7fcbf6
Nursizzy <101743904+Nursizzy@users.noreply.github.com>
2022-05-05 15:02:53 +0600

    Update script.js

--- 1st Home Work/script.js
+++ 1st Home Work/script.js
@@ -2,32 +2,31 @@
 //Selectors
 const FIRST_TASK_BUTTON = document.querySelector('.num_to_string');
 const SECOND_TASK_BUTTON = document.querySelector('.num_sum_and_num_division');
+
 //Functions
 const errorLog = () => console.log('Некорректный ввод!');
 
 //First task
 FIRST_TASK_BUTTON.addEventListener('click', function () {
   const num = +prompt('Введите число которое хотите преобразовать:');
-  const numSystem = +prompt(
-    'В какую систему исчисления хотите преобразовать число? Введите числовое значение от 2 до 36:'
-  );
+  const numSystem = +prompt('В какую систему исчисления хотите преобразовать число? \nВведите числовое значение от 2 до 36:');
+  
   if (
     isFinite(num) === isFinite(numSystem) &&
     numSystem >= 2 &&
-    numSystem <= 36
-  )
-    console.log(num.toString(numSystem));
+    numSystem <= 36) console.log(num.toString(numSystem));
   else errorLog();
 });
+
 //Second task
 SECOND_TASK_BUTTON.addEventListener('click', function () {
   const firstNum = +prompt('Введите первое число:');
+  
   if (isFinite(firstNum)) {
     const secondNum = +prompt('Введите второе число:');
-    if (isFinite(secondNum) && secondNum !== 0)
-      console.log(
-        `Ответ: ${firstNum + secondNum}, ${(firstNum / secondNum).toFixed(2)}`
-      );
+    
+    if (isFinite(secondNum) && secondNum !== 0) 
+      console.log(`Ответ: ${firstNum + secondNum}, ${(firstNum / secondNum).toFixed(2)}`);
     else errorLog();
   } else errorLog();
 });
