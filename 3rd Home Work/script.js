033a596f1fdd0ec4e66a339493bc122823b789c7
Nursizzy <101743904+Nursizzy@users.noreply.github.com>
2022-05-16 02:15:40 +0600

    Add files via upload

---
+++ 3rd Home Work/script.js
@@ -0,0 +1,39 @@
+///////////////// .....ДОМАШНЕЕ ЗАДАНИЕ №3.....\\\\\\\\\\\\\\\\\\\\\\\\
+// First Task:
+const testData = [1, 2, 3, 4, 5];
+
+Array.prototype.myFilter = function (fn, context = this) {
+  const result = [];
+
+  for (let i = 0; i < this.length; i++) {
+    if (fn.call(context, this[i], i, this)) {
+      result.push(this[i]);
+    }
+  }
+  return result;
+};
+// Testing
+const filteredData = testData.myFilter((x) => x > 2);
+console.log(filteredData); // output [3,4,5]
+
+// Second Task:
+const createDebounceFunction = function (fn, delay) {
+  let timeout;
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(fn, delay);
+  };
+};
+//Testing
+const log100 = () => console.log(100);
+const debounceLog100 = createDebounceFunction(log100, 1000);
+debounceLog100();
+setTimeout(debounceLog100, 200);
+setTimeout(debounceLog100, 400);
+// More testing
+setTimeout(debounceLog100, 500);
+setTimeout(debounceLog100, 1000);
+setTimeout(debounceLog100, 2000);
+setTimeout(debounceLog100, 3000);
+setTimeout(debounceLog100, 4000);
+setTimeout(debounceLog100, 5000);
