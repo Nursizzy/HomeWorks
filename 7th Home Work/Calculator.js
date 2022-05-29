1a59d1b2aa862a8a666f9421643a88cccf672555
Nursizzy <101743904+Nursizzy@users.noreply.github.com>
2022-05-30 02:23:40 +0600

    Add files via upload

---
+++ 7th Home Work/Calculator.js
@@ -0,0 +1,118 @@
+//Create Calculator class
+class Calculator {
+  constructor(previousNum, currentNum) {
+    this.previousNum = previousNum;
+    this.currentNum = currentNum;
+    this.clear();
+    this.memoryItem = 0;
+  }
+
+  clear() {
+    this.currentElem = '';
+    this.previousElem = '';
+    this.operation = undefined;
+  }
+
+  reverse() {
+    return (this.currentElem *= -1);
+  }
+
+  task() {
+    const prev = parseFloat(this.previousElem);
+    const current = parseFloat(this.currentElem);
+    let task;
+
+    if (isNaN(prev) || isNaN(current)) {
+      return;
+    }
+
+    switch (this.operation) {
+      case '+':
+        task = prev + current;
+        break;
+      case '-':
+        task = prev - current;
+        break;
+      case 'x':
+        task = prev * current;
+        break;
+      case '÷':
+        task = prev / current;
+        break;
+      case 'M+':
+        task = current;
+      default:
+        return;
+    }
+
+    this.currentElem = task;
+    this.operation = undefined;
+    this.previousElem = '';
+  }
+
+  delete() {
+    this.currentElem = this.currentElem.toString().slice(0, -1);
+  }
+
+  output(number) {
+    const stringNumber = number.toString();
+
+    if (stringNumber != null) {
+      let numOutput = number.toLocaleString('en', { maximumFractionDigits: 8 });
+
+      return `${numOutput}`;
+    }
+
+    return stringNumber;
+  }
+
+  display() {
+    this.currentNum.innerText = this.output(this.currentElem);
+    memoryDisplay.innerText = this.memoryItem;
+
+    if (this.operation != null) {
+      return (this.previousNum.innerText = `${this.output(this.previousElem)} ${
+        this.operation
+      }`);
+    }
+
+    return (this.previousNum.innerText = '');
+  }
+
+  addNum(number) {
+    if (number === '.' && this.currentElem.includes('.')) {
+      return;
+    }
+
+    this.currentElem = this.currentElem.toString() + number.toString();
+  }
+
+  operationClick(operation) {
+    if (this.currentElem === '') {
+      return;
+    } else if (this.previousElem !== '') {
+      this.task();
+    }
+
+    this.operation = operation;
+    this.previousElem = this.currentElem;
+    this.currentElem = '';
+  }
+
+  addSpecial(operation) {
+    switch (operation) {
+      case 'M+':
+        this.memoryItem = Math.abs(currentNum.innerText);
+        break;
+      case 'M-':
+        this.memoryItem = -Math.abs(currentNum.innerText);
+        break;
+      case 'MC':
+        this.memoryItem = 0;
+        break;
+      case 'MR':
+        this.currentElem = this.memoryItem;
+        break;
+    }
+  }
+}
---
+++ 7th Home Work/script.js
@@ -0,0 +1,90 @@
+// Declarations
+const numberBtns = document.querySelectorAll('.number');
+const operationBtns = document.querySelectorAll('.operation');
+const result = document.querySelector('.result');
+const deleteBtn = document.querySelector('.delete');
+const clear = document.querySelector('.clear');
+const previousNum = document.querySelector('.previousNum');
+const currentNum = document.querySelector('.currentNum');
+const reverseNum = document.querySelector('.operation-reverse');
+const favourtiesBtn = document.querySelector('.link');
+const memoryDisplay = document.querySelector('.memory');
+const specialBtns = document.querySelectorAll('.special');
+
+//Create calculator using Calculator class
+const calculator = new Calculator(previousNum, currentNum);
+
+// Onclick AddEventListeners
+numberBtns.forEach((button) => {
+  button.addEventListener('click', () => {
+    calculator.addNum(button.innerText);
+    calculator.display();
+  });
+});
+
+operationBtns.forEach((button) => {
+  button.addEventListener('click', () => {
+    calculator.operationClick(button.innerText);
+    calculator.display();
+  });
+});
+
+specialBtns.forEach((button) => {
+  button.addEventListener('click', () => {
+    calculator.addSpecial(button.innerText);
+    calculator.display();
+  });
+});
+
+result.addEventListener('click', () => {
+  calculator.task();
+  calculator.display();
+});
+
+clear.addEventListener('click', () => {
+  calculator.clear();
+  calculator.display();
+});
+
+deleteBtn.addEventListener('click', () => {
+  calculator.delete();
+  calculator.display();
+});
+
+reverseNum.addEventListener('click', () => {
+  calculator.reverse();
+  calculator.display();
+});
+
+// External function to bookmark page
+function addToFavourite(a) {
+  pageTitle = document.title;
+  pageURL = document.location;
+  try {
+    // Internet Explorer solution
+    eval('window.external.AddFa-vorite(pageURL, pageTitle)'.replace(/-/g, ''));
+  } catch (e) {
+    try {
+      // Mozilla Firefox solution
+      window.sidebar.addPanel(pageTitle, pageURL, '');
+    } catch (e) {
+      // Opera solution
+      if (typeof opera == 'object') {
+        a.rel = 'sidebar';
+        a.title = pageTitle;
+        a.url = pageURL;
+        return true;
+      } else {
+        // The rest browsers (i.e Chrome, Safari)
+        alert(
+          'Press ' +
+            (navigator.userAgent.toLowerCase().indexOf('mac') != -1
+              ? 'Cmd'
+              : 'Ctrl') +
+            '+D to bookmark this page.'
+        );
+      }
+    }
+  }
+  return false;
+}
